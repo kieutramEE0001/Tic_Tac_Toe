bd8dc93307bfb1847a73bcc16b43b88cbcbf1f39
Kieu Tram <Kieu Tram@LAPTOP-1RMFHO1A>
2024-12-13 09:46:34 +0700

    completed

--- Tic_Tac_Toe/Const.cs
+++ Tic_Tac_Toe/Const.cs
@@ -8,11 +8,11 @@ namespace Tic_Tac_Toe
 {
     internal class Const
     {
-        //Khai báo biến chiều dài và chiều cao của mỗi ô trong bàn cờ 
+        //Khai báo biến chiều rộng và chiều cao của mỗi ô trong bàn cờ 
         public static int Chess_W = 30;
         public static int Chess_H = 30;
 
-        //Khai báo biến chiều dài và chiều cao của bàn cờ
+        //Khai báo biến chiều rộng và chiều cao của bàn cờ
         public static int ChessBoard_W = 18;
         public static int ChessBoard_H = 18;
 
